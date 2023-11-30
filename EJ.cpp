8c9d6dd0bbf95f9ae7ed0654a28b96b63487080c
Sabil667 <msabi@myuax.com>
2023-11-30 12:46:47 +0100

    .

--- EJ.cpp
+++ EJ.cpp
@@ -9,3 +9,12 @@ int main() {
     std::cout << "Ingrese el nombre del archivo (sin extension): ";
     std::cin >> filename;
 
+    show_txt_files(filename);
+
+    return 0;
+}
+void show_txt_files(const std::string& filename) {
+    bool found = false;
+    std::string targetFilename = filename + ".txt";  // Agrega la extensión .txt
+
+
