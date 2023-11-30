7a3e1c0384e2083f37958c6ed8d200f1ae505ec1
Sabil667 <msabi@myuax.com>
2023-11-30 12:45:56 +0100

    .

--- EJ.cpp
+++ EJ.cpp
@@ -3,4 +3,9 @@
 #include <filesystem>
 
 void show_txt_files(const std::string& filename);
+int main() {
+    std::string filename;
+
+    std::cout << "Ingrese el nombre del archivo (sin extension): ";
+    std::cin >> filename;
 
